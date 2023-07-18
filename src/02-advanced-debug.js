80886fdb6799fb817c0dca91a60945079506beeb
BreElle Wells <breellejordyn@gmail.com>
2023-07-17 20:25:43 -0600

    build & iterate over nested objects

--- src/02-advanced-debug.js
+++ src/02-advanced-debug.js
@@ -1,28 +1,82 @@
-console.log('Advanced debugging example running.')
-debugger
+const game = gameObject();
 
-// first, define the function.
-function goodPractices() {
-  let game = gameObject();
-  for (let gameKey in game) {
-    // are you ABSOLUTELY SURE what 'gameKey' is?
-    // use the debugger to find out!
-    debugger
-    let teamObj = game[gameKey]
-    for (let teamKey in teamObj) {
-      // are you ABSOLUTELY SURE what 'teamKey' is?
-      // use debugger to find out!
-      debugger
+function numPointsScored(playerName) {
+    const allPlayers = { ...game.home.players, ...game.away.players };
+    const player = allPlayers[playerName];
 
-      // what is 'data' at each loop through out this block?
-      // when will the following line of code work and when will it break?
-      let data = teamObj.player
-      for (let key in data) {
-        debugger
-      }
+    if (player) {
+        return player.points;
     }
-  }
 }
 
-// then, call the function so it runs!
-goodPractices()
+function shoeSize(playerName) {
+    const allPlayers = { ...game.home.players, ...game.away.players };
+    const player = allPlayers[playerName];
+
+    if (player) {
+        return player.shoe;
+    }
+}
+
+function teamColors(teamName) {
+    for (let gameKey in game) {
+        let nameOfTeam = game[gameKey].teamName;
+        if (teamName === nameOfTeam) {
+            return game[gameKey].colors;
+        }
+    }
+}
+
+function teamNames() {
+    const names = [];
+    for (let gameKey in game) {
+        let nameOfTeam = game[gameKey].teamName;
+        names.push(nameOfTeam)
+    }
+    return names;
+}
+
+function playerNumbers(teamName) {
+    const numbers = [];
+    for (let gameKey in game) {
+        let nameOfTeam = game[gameKey].teamName;
+        if (teamName === nameOfTeam) {
+            const players = game[gameKey].players;
+            for (let playerName in players) {
+                let playerNumber = players[playerName].number;
+                numbers.push(playerNumber);
+            }
+        }
+    }
+    return numbers;
+}
+
+function playerStats(playerName) {
+    const allPlayers = { ...game.home.players, ...game.away.players };
+    const player = allPlayers[playerName];
+    return player;
+}
+
+function bigShoeRebounds() {
+    const allPlayers = { ...game.home.players, ...game.away.players };
+    const shoes = [];
+    
+    for (let playerName in allPlayers) {
+        const playerShoe = allPlayers[playerName].shoe
+        shoes.push(playerShoe)
+    }
+    
+    const bigShoePlayer = getPlayerByShoeSize(Math.max(...shoes));
+    return bigShoePlayer.rebounds;
+}
+
+function getPlayerByShoeSize(shoeSize) {
+    const allPlayers = { ...game.home.players, ...game.away.players };
+    
+    for (let playerName in allPlayers) {
+        const playerShoe = allPlayers[playerName].shoe
+        if (shoeSize === playerShoe) {
+            return allPlayers[playerName];
+        }
+    }
+}
